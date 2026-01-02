f214b6ba2687148d26a7788c15b9e2a70812d5b6
Emma Lejeck <emma@nuck.moe>
2026-01-01 18:53:58 -0800

    chore(launcher): Reformat App.axaml.cs

--- Galapa.Launcher/App.axaml.cs
+++ Galapa.Launcher/App.axaml.cs
@@ -20,11 +20,11 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit.
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
+            this.DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = Program.Services.GetRequiredService<MainWindow>();
         }
 
