6671f29be9185da4259564fa8e2f724dcaa869d5
Emma Lejeck <emma@nuck.moe>
2026-01-23 02:23:59 -0800

    feat(toolbox): Add more known filenames

--- Galapa.Toolbox/Models/KnownFile.cs
+++ Galapa.Toolbox/Models/KnownFile.cs
@@ -7,11 +7,17 @@ namespace Galapa.Toolbox.Models;
 
 public class KnownFile
 {
-    public required Func<Stream, Stream> Obfuscator;
-    public required string Name;
-
     public static readonly Dictionary<string, KnownFile> KnownFiles = new()
     {
+        { "BurakqOnn!pcs--!qca", new KnownFile { Name = "PresetPad.win32.xml", Obfuscator = FixedObfuscator.Factory } },
+        {
+            "ChqstfVjmkw!zmi--!vmh",
+            new KnownFile { Name = "PresetMouse.win32.xml", Obfuscator = FixedObfuscator.Factory }
+        },
+        {
+            "MghxsiOmzymxff!wvo$$!bvz",
+            new KnownFile { Name = "PresetKeyboard.win32.xml", Obfuscator = FixedObfuscator.Factory }
+        },
         { "FbrBrpgbkOhwj!ouc", new KnownFile { Name = "KeyConfigFile.xml", Obfuscator = FixedObfuscator.Factory } },
         { "cxjYxsgheGzie!iyx", new KnownFile { Name = "dqxPlayerList.xml", Obfuscator = UsernameObfuscator.Factory } },
         {
@@ -21,6 +27,19 @@ public class KnownFile
         {
             "YzjadjylzqvBmsvub!zni]]!vnh",
             new KnownFile { Name = "EnvironmentOption.win32.xml", Obfuscator = FixedObfuscator.Factory }
+        },
+        {
+            "shpkoLhdcPvwNmiitivstexqdlrcowjlXvnxry!dfj@@!qvr",
+            new KnownFile
+                { Name = "eventTextEtcWindowsgamepadmousesClient.win32.etp", Obfuscator = FixedObfuscator.Factory }
+        },
+        {
+            "aufjzAunbNycMdhhkhyczqfhgxmkyhSourjz!bjx$$!kop",
+            new KnownFile
+                { Name = "eventTextEtcWindowsconfigtoolsClient.win32.etp", Obfuscator = FixedObfuscator.Factory }
         }
     };
+
+    public required string Name;
+    public required Func<Stream, Stream> Obfuscator;
 }
